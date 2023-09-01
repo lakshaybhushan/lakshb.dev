cd9ee216bdecf03505a7a425bc8fb540993d35ed
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2023-09-01 22:54:21 +0530

    saturatio lvl thingy

--- src/components/home/Profile.jsx
+++ src/components/home/Profile.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Image from "next/image";
 const Profile = () => {
   return (
-    <div className="ImageContainer flex items-center justify-center">
+    <div className="ImageContainer mt-[2vh] flex h-full items-center justify-center overflow-hidden">
       <Image
         src="/images/me.webp"
         alt="Picture of the author"
         width={400}
         height={500}
-        className="h-full md:w-[24vw] mt-[2vh]"
-        id="ImageEffect"
+        className="saturate-0 transition-all duration-700 ease-in-out hover:saturate-[96%] md:w-[24vw]"
       />
     </div>
   );
