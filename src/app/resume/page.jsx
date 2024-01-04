c39e310179e47b2fe1830f6683dd56f8b4d2a93a
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-01-04 14:56:59 +0530

    resume under maintenance

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,29 +1,43 @@
-"use client";
-import React, { useEffect } from "react";
-import { motion } from "framer-motion";
-const page = () => {
-  useEffect(() => {
-    const isEmbedSupported = "src" in document.createElement("embed");
-    if (!isEmbedSupported) {
-      window.location.href = "https://urlify.lakshb.me/resume-web";
-    }
-  }, []);
+// "use client";
+// import React, { useEffect } from "react";
+// import { motion } from "framer-motion";
+// const page = () => {
+//   useEffect(() => {
+//     const isEmbedSupported = "src" in document.createElement("embed");
+//     if (!isEmbedSupported) {
+//       window.location.href = "https://urlify.lakshb.me/resume-web";
+//     }
+//   }, []);
 
+//   return (
+// <motion.div
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 1 }}
+//       transition={{ duration: 1.5 }}
+//       id="pdf-viewer"
+//       className="absolute inset-0 h-screen w-screen overflow-hidden">
+//       <embed
+//         src="docs/Resume.pdf"
+//         type="application/pdf"
+//         width="100%"
+//         height="100%"
+//         className="absolute inset-0 h-full w-full"
+//       />
+//     </motion.div>
+//   );
+// };
+
+// export default page;
+
+"use client";
+
+import React from "react";
+
+const page = () => {
   return (
-<motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      id="pdf-viewer"
-      className="absolute inset-0 h-screen w-screen overflow-hidden">
-      <embed
-        src="docs/Resume.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-        className="absolute inset-0 h-full w-full"
-      />
-    </motion.div>
+    <div className="flex items-center min-h-screen justify-center">
+      <p className="text-lg">under maintenance</p>
+    </div>
   );
 };
 
