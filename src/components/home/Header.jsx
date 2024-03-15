b978a95a9aa7e75b117d3b63d94868c9b9a8beef
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-03-15 14:31:12 +0530

    added WIP

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -3,6 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <main className="mt-[6vh] text-center md:mt-0">
+      <div className="mb-4">
+        <span class="me-2 rounded border bg-yellow-100 text-xs font-medium text-yellow-800 dark:border-yellow-800 dark:bg-yellow-950 dark:text-yellow-300 p-2 md:px-4 md:py-3 ">
+          🚧 Work in progress 🚧
+        </span>
+      </div>
       <h1 className="text-4xl">Lakshay Bhushan</h1>
       <p className="mt-2 text-light-other">(Personal Portfolio)</p>
     </main>
