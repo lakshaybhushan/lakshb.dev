3849a2194e08d8977b7b60fc06ad16cbe5447c05
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-05-29 13:42:16 +0530

    renamed blog to thoughts but I'll work on it later

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [react(), tailwind()],
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
+  output: "server",
+  integrations: [react(), tailwind(), mdx()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
