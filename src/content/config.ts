ea0621fe86af9fd6615a6cb16da6fa0056ab26d7
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-05-31 12:14:35 +0530

    image validation and new title

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,9 @@ const postsCollection = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			cover: image(),
 			date: z.coerce.date(),
 			excerpt: z.string(),
+			cover: image().refine((img)=>img.width >= 400, { message: "Cover image must be at least 400px wide" }),
 		}),
 });
 
